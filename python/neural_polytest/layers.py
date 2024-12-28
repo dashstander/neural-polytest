cb9525f51a065ce8b546a9b330e3f599eb1d180f
Dashiell Stander <dstander@protonmail.com>
2024-12-28 00:03:49 -0800

    Add transformer model
    Signed-off-by: Dashiell Stander <dstander@protonmail.com>

--- python/neural_polytest/layers.py
+++ python/neural_polytest/layers.py
@@ -172,7 +172,8 @@ class TransformerEncoderLayer(eqx.Module):
             key_size=d_model,
             value_size=d_model,
             key=attention_key,
-            inference=None
+            inference=None,
+            dropout=None
         )
         self.ff_linear_up = eqx.nn.Linear(d_model, d_ff, key=ff_key1)
         self.ff_linear_down = eqx.nn.Linear(d_ff, d_model, key=ff_key2)
